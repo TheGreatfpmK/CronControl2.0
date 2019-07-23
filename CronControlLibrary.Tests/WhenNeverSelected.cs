14f13eb0c8ecd934736e74db2fb7f0a9c8a29de3
Filip Macák <filip.macak1999@gmail.com>
2019-07-23 17:21:21 +0200

    Forgot to add test file

---
+++ CronControlLibrary.Tests/WhenNeverSelected.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ploeh.AutoFixture;
+
+namespace CronControlLibrary.Tests
+{
+    [TestClass]
+    public class WhenNeverSelected
+    {
+        private Fixture _fixture;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _fixture = new Fixture();
+        }
+
+        [TestMethod]
+        public void EmptyStringExpected()
+        {
+            // Arrange
+            var control = new CronControl();
+            control.SelectBoxIndex = 0;
+
+            // Assert
+            control.Value.Should().Be(string.Empty);
+
+        }
+    }
+}
